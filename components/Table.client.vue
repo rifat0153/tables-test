d72cb2e097273f6c12a73fcf6284183323605dc1
Mahbubur Rahman <rifat0153@gmail.com>
2024-02-26 19:29:30 +0000

    table working

--- components/Table.client.vue
+++ components/Table.client.vue
@@ -1,40 +1,15 @@
 <script setup lang="ts">
-import { ref } from "vue";
+const items = generateDummyObjects(150);
 
-interface Cell {
-  value: boolean;
-  originalValue: boolean;
-  id: string;
-  // Add other properties as needed
-}
-
-const columns = ref(
-  Array.from({ length: 25 }, (_, index) => `Column ${index + 1}`)
-);
-const rows = ref(
-  Array.from({ length: 150 }, (_, rowIndex) => {
-    return Array.from({ length: 25 }, (_, cellIndex) => ({
-      value: false,
-      originalValue: false,
-      id: `${rowIndex}-${cellIndex}`,
-      // Add other properties as needed
-    }));
-  })
-);
-
-const changedItemsCount = ref(0);
-
-function updateCellValue(rowIndex: number, cellIndex: number) {
-  const cell = rows.value[rowIndex][cellIndex];
-  if (cell.value !== cell.originalValue) {
-    if (cell.value) {
-      changedItemsCount.value++;
-    } else {
-      changedItemsCount.value--;
-    }
-  }
-  cell.originalValue = cell.value;
-}
+const { changedItemsCount, columns, rows, updateCellValue } =
+  useTable<LocationMapping>(
+    items,
+    "locationId",
+    "locationName",
+    "triggerId",
+    "triggerName",
+    "mappingId"
+  );
 </script>
 
 <template>
--- composables/useTable.ts
+++ composables/useTable.ts
@@ -1,27 +1,117 @@
-interface Cell {
+export type Cell = {
+  id: string;
+  columnId: number;
+  rowId: number;
   value: boolean;
   originalValue: boolean;
-  id: string;
-  // Add other properties as needed
-}
+};
 
-export const useTable = () => {
-  const columns = ref(
-    Array.from({ length: 25 }, (_, index) => `Column ${index + 1}`)
-  );
-  const rows = ref(
-    Array.from({ length: 150 }, (_, rowIndex) => {
-      return Array.from({ length: 25 }, (_, cellIndex) => ({
-        value: false,
-        originalValue: false,
-        id: `${rowIndex}-${cellIndex}`,
-        // Add other properties as needed
-      }));
-    })
-  );
+export type MappingItem = {
+  triggerId: number;
+  triggerName: string;
+  locationId: number;
+  locationName: string;
+  mappingId: number | null;
+  mappingDescription: string;
+};
+
+export type LocationMapping = {
+  triggerId: number;
+  triggerName: string;
+  locationId: number;
+  locationName: string;
+  mappingId: number | null;
+  mappingDescription: string;
+};
+
+export const useTable = <T extends MappingItem>(
+  _items: MaybeRefOrGetter<T[]>,
+  rowIdField: keyof T,
+  rowNameField: keyof T,
+  columnIdField: keyof T,
+  columnNameField: keyof T,
+  mappingIdField: keyof T
+) => {
+  const changedItemsCount = ref(0);
+
+  const items = toRef(_items) as Ref<T[]>;
+
+  const columns = ref<string[]>([]);
+  const rows = ref<Cell[][]>([]);
+
+  function generateTable() {
+    const uniqueColumns = new Set<number>();
+    const uniqueRows = new Set<number>();
+
+    items.value.forEach((item) => {
+      uniqueColumns.add(item[columnIdField] as number);
+      uniqueRows.add(item[rowIdField] as number);
+    });
+
+    const r = [...uniqueRows];
+    const c = [...uniqueColumns];
+
+    columns.value = Array.from(uniqueColumns).map((id) => `Column ${id}`);
+
+    const rowArr = Array.from(uniqueRows);
+
+    for (let i = 0; i < r.length; i++) {
+      const row: Cell[] = [];
+
+      const rowId = r[i];
+
+      for (let j = 0; j < c.length; j++) {
+        const columnId = c[j];
+
+        const cell: Cell = {
+          id: `${rowId}-${columnId}`,
+          rowId: rowId,
+          columnId: columnId,
+          value: false,
+          originalValue: false,
+        };
+        row.push(cell);
+      }
+      rows.value.push(row);
+    }
+  }
+
+  function updateCellValue(rowIndex: number, columnIndex: number) {
+    const cell = rows.value[rowIndex][columnIndex];
+    if (cell.value !== cell.originalValue) {
+      if (cell.value) {
+        changedItemsCount.value++;
+      } else {
+        changedItemsCount.value--;
+      }
+    }
+    cell.originalValue = cell.value;
+  }
+
+  generateTable();
 
   return {
     columns,
     rows,
+    changedItemsCount,
+    updateCellValue,
   };
 };
+
+export function generateDummyObjects(n: number): LocationMapping[] {
+  const dummyObjects: LocationMapping[] = [];
+  for (let i = 1; i <= n; i++) {
+    const id = i + 1000;
+
+    const dummyObject: LocationMapping = {
+      triggerId: id + 5000,
+      triggerName: `Trigger ${id + 5000}`,
+      locationId: id,
+      locationName: `Location ${id}`,
+      mappingId: id % 2 === 0 ? null : id,
+      mappingDescription: `Mapping ${id}`,
+    };
+    dummyObjects.push(dummyObject);
+  }
+  return dummyObjects;
+}
